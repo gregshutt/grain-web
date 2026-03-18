0e1bd1f59a3449d2145ce5817815bda46e19cc24
Greg Shutt <github@gregshutt.com>
2026-03-18 14:23:43 -0400

    first commit

---
+++ grain/extensions.py
@@ -0,0 +1,5 @@
+from flask_debugtoolbar import DebugToolbarExtension
+from flask_static_digest import FlaskStaticDigest
+
+debug_toolbar = DebugToolbarExtension()
+flask_static_digest = FlaskStaticDigest()
