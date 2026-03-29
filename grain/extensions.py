ce2744627f48eab876505c9f0cd34ba0002f18c2
Greg Shutt <github@gregshutt.com>
2026-03-29 10:00:28 -0400

    moving medical loading out

--- grain/extensions.py
+++ grain/extensions.py
@@ -1,9 +1,5 @@
 from flask_debugtoolbar import DebugToolbarExtension
 from flask_static_digest import FlaskStaticDigest
-from config import settings
 
 debug_toolbar = DebugToolbarExtension()
 flask_static_digest = FlaskStaticDigest()
-
-# load up the medical library
-settings.get_medical()
